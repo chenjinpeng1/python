9fe5444097b8b2a1077e0752ee83a89a002077ee
chenjinpeng <1585742649@qq.com>
2016-01-05 23:04:30 +0800

    day1

--- day1/City_Select/City_select.py
+++ day1/City_Select/City_select.py
@@ -2,89 +2,56 @@ import CityList
 Num=['0','1','2','3']
 SelectSuccess=True
 while SelectSuccess:
-    if SelectSuccess==False:
-        break
     City_List=list(CityList.info.keys())
-    b = ''
+    Print_City = ''
     Key = enumerate(CityList.info.keys())
     for i,val in Key:
-        i = str(i)
-        b += i + '\t' + val + '  |  '
-    print (b)
-    # print ('''
-    # 北京
-    # 上海
-    # 广州
-    # 深圳
-    # ''')
-    City = input('请输入序列号选择你要查询的城市，Q退出:').strip()
+      i = str(i)
+      Print_City += i + '\t' + val + '  |  '
+    print ('\033[36;1m欢迎登陆查询系统，请根据提示进行查询\033[0m')
+    print ('\033[36;1m%s\033[0m'%(Print_City))
+    City=input('\033[36;1m请输入序列号选择你要查询的城市，Q退出:\033[0m').strip()
     if City == "Q":
         SelectSuccess=False
         break
-    if City == '0':
-        Area_List = list(CityList.info[City_List[0]])
-        b = ''
-        Key = enumerate(CityList.info[City_List[0]].keys())
+    if len(City) != 0 and int(City) < len(City_List):
+        Area_List = list(CityList.info[City_List[int(City)]])
+        Print_Area = ''
+        Key = enumerate(CityList.info[City_List[int(City)]].keys())
         for i,val in Key:
             i = str(i)
-            b += i + '\t' + val + '  |  '
+            Print_Area +=i + '\t' + val + ' | '
         while SelectSuccess:
-            A=input  ('''
+            User_Input=input  ('''\033[36;1m
     ===============================================================================================
                                            欢迎来到 %s:
                                          %s区域分布如下：
      %s
     ===============================================================================================
-    请输入您选择的城市编号，按Q退出,B返回上一级:
-    '''%(City,City,b))
-            if A == 'B':
+    请输入您选择的城市编号，按Q退出,B返回上一级:\033[0m
+    '''%(City_List[int(City)],City_List[int(City)],Print_Area)).strip()
+            if User_Input == 'B':
                 break
-            if A == 'Q':
+            if User_Input == 'Q':
                 SelectSuccess=False
                 break
-            if A == "0":
-                print (CityList.info['北京'][Area_List[0]])
-            if A == "1":
-                print (CityList.info['北京'][Area_List[1]])
-            if A == "2":
-                print (CityList.info['北京'][Area_List[2]])
-            if A == "3":
-                print (CityList.info['北京'][Area_List[3]])
-            if A == "4":
-                print (CityList.info['北京'][Area_List[4]])
-    # if City == '上海':
-    #     List = list(CityList.info['上海'])
-    #     b = ''
-    #     bj_key = enumerate(CityList.info['上海'].keys())
-    #     for i,val in bj_key:
-    #         i = str(i)
-    #         b += i + '\t' + val + '  |  '
-    #     while SelectSuccess:
-    #         A=input  ('''
-    # ===============================================================================================
-    #                                        欢迎来到 %s:
-    #                                      %s区域分布如下：
-    #  %s
-    # ===============================================================================================
-    # 请输入您选择的城市编号，按Q退出,B返回上一级:
-    # '''%(City,City,b))
-    #         if A == 'B':
-    #             break
-    #         if A == 'Q':
-    #             SelectSuccess=False
-    #             break
-    #         if A == "0":
-    #             print (CityList.info['上海'][List[0]])
-    #         if A == "1":
-    #             print (CityList.info['上海'][List[1]])
-    #         if A == "2":
-    #             print (CityList.info['上海'][List[2]])
-    #         if A == "3":
-    #             print (CityList.info['上海'][List[3]])
-    #         if A == "4":
-    #             print (CityList.info['上海'][List[4]])
-
-
+            if len(User_Input) != 0 and int(User_Input) < len(Area_List):
+                if int(User_Input) <= len(Area_List):
+                    print ('\033[36;1m')
+                    print (CityList.info[City_List[int(City)]][Area_List[int(User_Input)]])
+                    print ('\033[0m')
+                    Exit=input('\033[36;1m查询结束，Q退出，任意键继续查询\033[0m')
+                    if Exit == 'Q':
+                        SelectSuccess=False
+                        break
+                else:
+                    print ('\033[1;31;40m请输入正确的序列号\033[')
+            else:
+                print ('\033[1;31;40m请输入正确的序列号\033[0m')
+    else:
+        print ('\033[1;31;40m请输入正确的序列号\033[0m')
+if SelectSuccess==False:
+       print ('\033[36;1m谢谢使用，再见！\033[0m')
 
 
 
